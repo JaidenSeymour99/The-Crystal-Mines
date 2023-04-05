e7e2a5439014807096f15a94ee5254b2a1227602
JoshuaSeymour99 <j0shuadan1elseym0ur@gmail.com>
2023-04-05 19:27:05 +0100

    WALL JUMP WORKING!
    Changed a lot of code, tidied up a lot of the update code and a lot of the animation code. and got wall jump working.

--- Metroidvania/Assets/Scripts/PlayerMvt.cs
+++ Metroidvania/Assets/Scripts/PlayerMvt.cs
@@ -9,7 +9,8 @@ using UnityEngine.InputSystem;
 public class PlayerMvt : MonoBehaviour
 {
     [Header("Movement Details")]
-    [SerializeField] private float speed = 8.0f;
+    [SerializeField] private float maxSpeed = 8.0f;
+    private float speed;
     private float direction;
     private bool facingRight = true;
 
@@ -18,17 +19,20 @@ public class PlayerMvt : MonoBehaviour
 
     [Header("Jump Details")]
     [SerializeField]private float jumpForce = 10.0f;
-    private bool stoppedJumping;
+    
 
     private float coyoteTime = 0.2f;
     private float coyoteTimeCounter;
 
-    private bool isWallJumping;
+    private bool isJumping;
+    private bool isFalling; 
+
+    [SerializeField]private bool isWallJumping;
     private float wallJumpingDirection;
-    private float wallJumpingTime = 0.2f;
+    private float wallJumpingTime = 0.1f;
     private float wallJumpingCounter;
-    private float wallJumpingDuration = 0.4f;
-    private Vector2 wallJumpingPower = new Vector2(8f,16f);
+    private float wallJumpingDuration = 0.1f;
+    [SerializeField]private Vector2 wallJumpingPower = new Vector2(3f,7f);
  
     [SerializeField]private float jumpsLeft;
     [SerializeField]private float maxJumps = 2f;
@@ -55,50 +59,255 @@ public class PlayerMvt : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
 
+        wallJumpingCounter = wallJumpingDuration;
         jumpsLeft = maxJumps;  
-
+        speed = maxSpeed;
     }
 
     //method that repeats every frame used to check if the player is touching the ground and is facing the right direction.
     //handles the movement of the player.
     private void Update()
     {
+        if(rb.velocity.y > 0.1f)
+        {
+            isJumping = true;
+        }
+        else 
+        {
+            isJumping = false;
+        }
+
+        if(rb.velocity.y < -0.1f)
+        {
+            isFalling = true;
+
+        }
+        else 
+        {
+            isFalling = false;
+        }
+        
         //if the player is on the ground the falling anim will be false. 
         if (IsGrounded())
         {
-            myAnimator.SetBool("falling", false);
-            myAnimator.SetBool("walled", false);
             jumpsLeft = maxJumps;
-
+            
             coyoteTimeCounter = coyoteTime;
         }
         else 
         {
             coyoteTimeCounter -= Time.deltaTime;
         }
-        //when the player is falling play anims
-        if(rb.velocity.y < 0f)
-        {
-            if(IsWalled())
-            {
-                WallSlide();
-            }
-            else
-            {
-                myAnimator.SetBool("walled", false);
-                myAnimator.SetBool("falling", true);
-            }
 
-            //this is added to make sure the falling animation isnt stuck if the player holds the button while moving.
-            if(IsGrounded())
-            {
-                myAnimator.SetBool("falling", false);
-                myAnimator.SetBool("walled", false);
-                myAnimator.ResetTrigger("jump"); 
-                isWallSliding = false;
-            }
+        if (IsWalled())
+        {
+            
+            wallJumpingCounter = wallJumpingDuration;
         }
+        else 
+        {
+            wallJumpingCounter -= Time.deltaTime;
+        }
+        //when the player is falling play anims
+        
         //making sure the player is facing the correct direction.
+        if(!isWallJumping)
+        {
+            IsMoving();
+            WallSlide();
+            IsFalling();
+            IsWalling();
+            IsJumping();
+            ChangeDirection();
+        }
+        else
+        {
+            
+            IsMoving();
+            
+        }
+
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if(!isWallJumping)
+        {
+            //controlling the movement of the player, changing the x velocity.
+            rb.velocity = new Vector2(direction * speed, rb.velocity.y);
+        }
+    }
+
+    //using the new input system to control the player jump.
+    public void Jump(InputAction.CallbackContext context)
+    {
+        
+        //Jump from the wall
+        if (context.performed && wallJumpingCounter > 0f)
+        {
+            wallJumpingDirection = -transform.localScale.x;
+            rb.velocity = new Vector2(wallJumpingDirection * wallJumpingPower.x, wallJumpingPower.y);
+            isJumping = true;
+            isFalling = false;
+            isWallSliding = false;
+            isWallJumping = true;
+
+            wallJumpingCounter = 0;
+
+        }
+        //using a jump after the player leaves the wall
+        else if (context.performed && jumpsLeft > 0f && !isWallSliding)
+        {
+            isJumping = true;
+            //adds a velocity (jump force) to the y value of the rigid body.
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            
+            //the coyote timer makes it so there is some leaniency with jumping if you have just left the ground and the jump button is pressed the player will still jump. 
+            //taking away 1 jump from jumps left.   
+            if(coyoteTimeCounter <= 0f)
+            {
+                jumpsLeft -= 1f;
+            }
+                        
+        }
+        else if (context.performed && jumpsLeft > 0f)
+        {
+            
+            isJumping = true;
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            isWallSliding = false;
+
+            jumpsLeft -= 1f;
+        }
+        
+        //when jump is pressed and jumps left is more than 0.
+        //Normal jump without wall
+        //when jump is canceled.
+        else if (context.canceled && rb.velocity.y > 0f)
+        {
+            isJumping = false;
+            isWallJumping = false;
+            //allowing the player to jump higher by pressing jump for longer and lower by pressing it for a short time.
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+
+            coyoteTimeCounter = 0f;
+        }
+        else if (context.canceled) 
+        {
+            isJumping = false;
+            isWallJumping = false;
+            coyoteTimeCounter = 0f;
+        }
+
+    }
+
+    //finding the direction the player is trying to move
+    public void Move(InputAction.CallbackContext context)
+    {
+        
+        if(!isWallJumping)
+        {
+            //when the player moves.
+            if(context.performed && IsWalled())
+            {
+                direction = context.ReadValue<Vector2>().x;     
+                isWallSliding = true;           
+            }
+            if(context.performed)
+            {
+                direction = context.ReadValue<Vector2>().x;
+                
+            }
+            //when the player cancels their movement.
+            else if(context.canceled)
+            {
+                direction = context.ReadValue<Vector2>().x;
+                
+            }    
+
+        }
+        else
+        {
+            direction = context.ReadValue<Vector2>().x; 
+            isWallSliding = false;
+        }
+
+    }
+
+    private void WallSlide()
+    {
+        if(IsWalled() && !IsGrounded() && (direction > 0f || direction < 0f)) 
+        {
+            isWallSliding = true;
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -wallSlidingSpeed, float.MaxValue));
+            
+        }
+        else
+        {
+            isWallSliding = false;
+
+        }
+    }
+
+    private void IsMoving()
+    {
+        myAnimator.SetFloat("speed", Mathf.Abs(direction));
+    }
+    private void IsWalling()
+    {
+        if (isWallSliding && isJumping)
+        {
+            myAnimator.SetTrigger("jump");
+            myAnimator.SetBool("walled", false);
+        }
+        else if (isWallSliding)
+        {
+            myAnimator.SetBool("walled", true);
+
+        }
+        else
+        {
+            myAnimator.SetBool("walled", false);
+        }
+    }
+    
+    private void IsJumping()
+    {
+        if (isJumping && (isWallSliding || isWallJumping))
+        {
+            myAnimator.SetTrigger("jump");
+            myAnimator.SetBool("walled", false);
+        }
+        else if (isJumping)
+        {
+            myAnimator.SetTrigger("jump");
+
+        }
+        else 
+        {
+            myAnimator.ResetTrigger("jump");
+        }
+    }
+    private void IsFalling()
+    {
+        if (isFalling && isWallSliding)
+        {
+            myAnimator.SetBool("walled", true);
+            myAnimator.SetBool("falling", true);
+        }
+        else if (isFalling)
+        {
+            myAnimator.SetBool("falling", true);
+        }
+        else
+        {
+            myAnimator.SetBool("falling", false);
+        }
+    }
+
+    private void ChangeDirection()
+    {
         if (!facingRight && direction > 0f)
         {
             Flip();
@@ -107,103 +316,6 @@ public class PlayerMvt : MonoBehaviour
         else if(facingRight && direction < 0f)
         {
             Flip();
-        }
-        //controlling the movement of the player.
-        rb.velocity = new Vector2(direction * speed, rb.velocity.y);
-
-        
-    }
-
-    private void FixedUpdate()
-    {
-        
-    }
-
-    //using the new input system to control the player jump.
-    public void Jump(InputAction.CallbackContext context)
-    {
-        //when jump is pressed and jumps left is more than 0.
-        if (context.performed && jumpsLeft > 0f && !isWallSliding)
-        {
-            //adds a velocity (jump force) to the y value of the rigid body.
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-            myAnimator.SetTrigger("jump");
-            myAnimator.SetBool("walled", false);
-            myAnimator.SetBool("falling", false);
-            
-            //the coyote timer makes it so there is some leaniency with jumping if you have just left the ground and the jump button is pressed the player will still jump. 
-            //taking away 1 jump from jumps left.   
-            if(coyoteTimeCounter <= 0f)
-            {
-                jumpsLeft -= 1f;
-            }
-        } 
-        else if (context.performed && isWallSliding)
-        {
-
-        }
-        //when jump is canceled.
-        else if (context.canceled && rb.velocity.y > 0f)
-        {
-            //allowing the player to jump higher by pressing jump for longer and lower by pressing it for a short time.
-            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
-            myAnimator.ResetTrigger("jump");
-            myAnimator.SetBool("falling", true);
-
-            coyoteTimeCounter = 0f;
-        }
-
-
-    }
-
-    //finding the direction the player is trying to move
-    public void Move(InputAction.CallbackContext context)
-    {
-        direction = context.ReadValue<Vector2>().x;
-        myAnimator.SetFloat("speed", Mathf.Abs(direction));
-
-        
-    }
-    //bool to check if the player is on the ground. returns true or false.
-    private bool IsGrounded()
-    {
-        //drawing a small circle under the rigid body to check if its touching the ground mask. if it is return true. if not return false.
-        return Physics2D.OverlapCircle(groundCheck.position, radOfCircle, groundMask);
-    }
-
-    private bool IsWalled()
-    {
-        return Physics2D.OverlapCircle(wallCheck.position, radOfCircle, wallLayer);
-    }
-
-    private void WallSlide()
-    {
-        if(IsWalled() && !IsGrounded() && (direction > 0f || direction < 0f)) 
-        {
-            isWallSliding = true;
-            myAnimator.SetBool("walled", true);
-            myAnimator.ResetTrigger("jump"); 
-            rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -wallSlidingSpeed, float.MaxValue));
-        }
-        else
-        {
-            isWallSliding = false;
-            myAnimator.SetBool("walled", false);
-            myAnimator.ResetTrigger("jump"); 
-        }
-    }
-
-    private void WallJump()
-    {
-        if(isWallSliding)
-        {
-            isWallJumping = false;
-            wallJumpingDirection = -transform.localScale.x;
-            wallJumpingCounter = wallJumpingTime;
-        }
-        else
-        {
-            wallJumpingCounter -= Time.deltaTime;
         }
     }
     //method used to change the direction a rigid body is facing 
@@ -214,6 +326,18 @@ public class PlayerMvt : MonoBehaviour
             Vector3 theScale = transform.localScale;
             theScale.x *= -1;
             transform.localScale = theScale;
+    }
+    //bool to check if the player is on the ground. returns true or false.
+    private bool IsGrounded()
+    {
+        //drawing a small circle under the rigid body to check if its touching the ground mask. if it is return true. if not return false.
+        return Physics2D.OverlapCircle(groundCheck.position, radOfCircle, groundMask);
+    }
+
+    //bool. checking for player / wall overlap. returns true or false.
+    private bool IsWalled()
+    {
+        return Physics2D.OverlapCircle(wallCheck.position, radOfCircle, wallLayer);
     }
 
     //used to draw a gizmo that is visible to the editor but not in game.
