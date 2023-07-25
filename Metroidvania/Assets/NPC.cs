d402f3ad005bc8e667a121191ebd0d3a68c5345d
JoshuaSeymour99 <j0shuadan1elseym0ur@gmail.com>
2023-07-25 11:54:02 +0100

    Adding Text boxes
    Text boxes and character dialogue, adding NPC Script which will deal with the dialogue and which text to display

---
+++ Metroidvania/Assets/NPC.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NPC : MonoBehaviour
+{
+    public GameObject dialoguePanel;
+    public Text dialogueText;
+    public string[] dialogue;
+    private int index;
+
+    public float wordSpeed;
+    public bool playerIsClose;
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            playerIsClose = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            playerIsClose = false;
+        }
+    }
+}
