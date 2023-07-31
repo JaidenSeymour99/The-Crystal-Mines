0b9d56487f6d8427bebcd19de2682a916b6d25ed
JoshuaSeymour99 <j0shuadan1elseym0ur@gmail.com>
2023-07-31 12:03:26 +0100

    Settings Set up
    Can control volume, change resolution and graphics

--- Metroidvania/Assets/Scripts/SettingsMenu.cs
+++ Metroidvania/Assets/Scripts/SettingsMenu.cs
@@ -1,18 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class SettingsMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    Resolution[] resolutions;
+    public TMP_Dropdown resolutionDropdown;
+    public AudioMixer audioMixer;
+
     void Start()
+
     {
-        
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+        int currentResolutionIndex = 0;
+
+        List<string> options = new List<string>();
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if(resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetVolume(float volume)
     {
-        
+        audioMixer.SetFloat("mastervolume", volume);
     }
+
+    public void SetQuality (int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
 }
