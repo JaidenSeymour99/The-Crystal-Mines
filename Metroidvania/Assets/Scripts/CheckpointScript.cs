65446ce7fdea1a88dcaa36fbd3e1ce14ae155155
JoshuaSeymour99 <j0shuadan1elseym0ur@gmail.com>
2023-07-31 04:25:25 +0100

    Added respawn, and checkpoints

---
+++ Metroidvania/Assets/Scripts/CheckpointScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointScript : MonoBehaviour
+{
+    private RespawnScript respawn;
+
+    void Awake()
+    {
+        respawn = GameObject.FindGameObjectWithTag("Respawn").GetComponent<RespawnScript>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            respawn.respawnPoint = this.gameObject;
+        }
+    }
+
+}
